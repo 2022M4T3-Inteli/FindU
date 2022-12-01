ee81f00a8c0297d49b5bf423e952da6a20e2d309
pedro123483 <pg308758@gmail.com>
2022-12-01 17:00:41 -0300

    lalallala

--- src/Frontend/FrontInicial - Sprint 4/codeFront.js
+++ src/Frontend/FrontInicial - Sprint 4/codeFront.js
@@ -1,51 +1,79 @@
-var beacons = [];
+const beacons = [];
 const tags = [];
-
-function inicializacao() {
-    //confirm("Oi");
-    var tela = L.map('tela').setView([5, 0], 4);
-    L.tileLayer('https://images.tcdn.com.br/img/img_prod/1102995/papel_de_parede_grid_azul_marinho_33_1_93a5945ec5a8fdbd1fd2ee6d69ae0faf.jpg', {
-        maxZoom: 4,
-        attribution: ''
+let r = -1
+const grupos = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var mapaTextura = localStorage.getItem('img');
+var input_obj = document.getElementById('mapaForm')
+var listaB = document.getElementById("listas_Beacon");
+if(ReferenceError){
+    localStorage.clear();
+}
+function inicializacao(){
+    var allBeacon
+    var tela = L.map('tela').setView([5,0], 4);
+    L.tileLayer('imagens/bg_map.png', {
+    maxZoom: 1,
+    attribution: ''
     }).addTo(tela);
+    var tagDiv = L.divIcon({className: 'my-div-icon',html:'<div class="tag" style="background: #FF0000;"></div>'});
     // requisição ajax que retorna todos os projetos cadastrados no banco de dados
     let ajax = new XMLHttpRequest();
     ajax.open("GET", "https://45gxmd-3000.preview.csb.app/tag", true);
     ajax.onreadystatechange = () => {
-        if (ajax.status == 200 && ajax.readyState == 4) {
+        if (ajax.status == 200 && ajax.readyState == 4){
+            var beacon_num = 0
+            var grupo
+            var grupo_num = 0
             let dados = JSON.parse(ajax.responseText);
             for (let i = 0; i < dados.length; i++) {
-                beacons.push(dados[i]['positionX']);
-                beacons.push(dados[i]['positionY']);
-                beacons.push(dados[i]['positionZ']);
-                console.log(dados);
-                tags[i] = L.marker([0, 0], { draggable: false }).addTo(tela).bindPopup(`Status: ${dados[0].status}<br>Nome: ${dados[0].name}<br>Categoria: ${dados[0].category}<br>Posição A1: ${dados[0].positionX}<br>Posição A2: ${dados[0].positionY}<br>Posição A3: ${dados[0].positionZ}`);
+            beacons.push(dados[i]['positionX']);
+            beacons.push(dados[i]['positionY']);
+            beacons.push(dados[i]['positionZ']);
+            console.log(dados);
+            tags[i] = L.marker([i*16,i*16],{icon:tagDiv,draggable:false}).addTo(tela).bindPopup(`Status: ${dados[i].status}<br>Nome: ${dados[i].name}<br>Categoria: ${dados[i].category}<br>Posição A1: ${dados[i].positionX}<br>Posição A2: ${dados[i].positionY}<br>Posição A3: ${dados[i].positionZ}`);
             }
             for (let i = 0; i < beacons.length; i++) {
-                let myIcon = L.icon({
-                    iconUrl: `../beaconImage/A${i + 1}.png`,
-                    iconSize: [32, 32],
-                    iconAnchor: [22, 94],
-                    popupAnchor: [-3, -76],
-                    shadowUrl: '../beaconImage/A1_shadow.png',
-                    shadowSize: [38, 38],
-                    shadowAnchor: [22, 94]
-                });
-                if (i % 2 == 0) {
-                    let beacon = L.marker([-5 * i, -10], { icon: myIcon, draggable: true }).addTo(tela);
+                beacon_num += 1
+                if (i % 3 == 0){
+                    grupo = grupos[grupo_num]
+                    beacon_num = 1
+                    grupo_num +=1
+                }
+                console.log(grupo)
+                var myIcon = L.divIcon({className: 'my-div-icon',html:`<div class="icon_beacons">${grupo}${beacon_num}</div>`});
+                if (i % 2 ==0) {
+                    let beacon = L.marker([-5*i,-10],{icon:myIcon,draggable:true}).addTo(tela);
                 } else {
-                    let beacon = L.marker([-10, 5 * 2], { icon: myIcon, draggable: true }).addTo(tela);
+                    let beacon = L.marker([-10,5*2],{icon:myIcon,draggable:true}).addTo(tela);
                 }
             }
             if (beacons.length >= 0) {
-                var listaB = document.getElementById("listas_Beacon");
-
-                for (let i = 0; i < (beacons.length) / 3; i++) {
-                    listaB.innerHTML += `<ul id="lista_Beacon"><li><div class="quadrado">${"A" + 1}</div> <div class="beancon" style="color:brown;"><span style="color: black;" class="material-symbols-outlined">arrow_right_alt</span> <span style="color: black;">${dados[0]['positionX']} M</span>  ${"9%"}<span class="material-symbols-outlined">${"battery_very_low"}</span></div></li><li><div class="quadrado">${"A" + 2}</div> <div class="beancon" style="color:brown;"><span style="color: black;" class="material-symbols-outlined">arrow_right_alt</span> <span style="color: black;">${dados[0]['positionY']} M</span>  ${"11%"}<span class="material-symbols-outlined">${"battery_very_low"}</span></div></li><li><div class="quadrado">${"A" + 3}</div> <div class="beancon" style="color:brown;"><span style="color: black;" class="material-symbols-outlined">arrow_right_alt</span> <span style="color: black;">${dados[0]['positionZ']} M</span>  ${"100%"}<span class="material-symbols-outlined">${"battery_full_alt"}</span></div></li></ul>`;
-
+                for (let a = 0; a < (beacons.length)/3; a++) {
+                    for (let i = 0; i < tags.length; i++) {
+                        allBeacon += `<ul class="lista_Beacon">
+                        <li><div class="quadrado">${grupos[i]}1</div> <div class="beancon" style="color:brown;"><span style="color: black;" class="material-symbols-outlined">arrow_right_alt</span> <span style="color: black;">${dados[i]['positionX']} M</span>  ${"9%"}<span class="material-symbols-outlined">${"battery_very_low"}</span></div></li>
+                        <li><div class="quadrado">${grupos[i]}2</div> <div class="beancon" style="color:brown;"><span style="color: black;" class="material-symbols-outlined">arrow_right_alt</span> <span style="color: black;">${dados[i]['positionY']} M</span>  ${"11%"}<span class="material-symbols-outlined">${"battery_very_low"}</span></div></li>
+                        <li><div class="quadrado">${grupos[i]}3</div> <div class="beancon" style="color:brown;"><span style="color: black;" class="material-symbols-outlined">arrow_right_alt</span> <span style="color: black;">${dados[i]['positionZ']} M</span>  ${"100%"}<span class="material-symbols-outlined">${"battery_full_alt"}</span></div></li>
+                        </ul>`;
+                        r += 1
+                    }
+                    allBeacon =  `<div class="grupo">${allBeacon}</div>`;
+                    listaB.innerHTML = allBeacon;
+                    allBeacon = '';
                 }
             }
         }
     }
     ajax.send();
 }
+
+input_obj.addEventListener('change',function(e){
+    let inputTarget = e.target;
+    let file = inputTarget.files[0];
+    if(file){
+        localStorage.clear()
+        mapaTextura = URL.createObjectURL(file);
+        localStorage.setItem('img',mapaTextura);
+        document.getElementById('tela').style.backgroundImage = `url(${mapaTextura})`
+    }
+});
--- src/Frontend/FrontInicial - Sprint 4/configuracao.js
+++ src/Frontend/FrontInicial - Sprint 4/configuracao.js
@@ -7,78 +7,71 @@ ajax.onreadystatechange = () => {
         let dados = JSON.parse(ajax.responseText);
         console.log(dados);
         for (let i = 0; i < dados.length; i++) {
-            // tabela_configuracao.append('<tr> <th> predroor </th></tr>');
-            $("#corpo-tabela-tags")[0].innerHTML += `
-            <tr id="tag_${dados[i]._id}">
+            $("#corpo-tabela-tags").append(`<tr id="tag_${dados[i]._id}">
                 <td>${dados[i].name}</td>
-                <td> <span class="${dados[i].category.color}"> ${dados[i].category.name} </td>
-                <td> <ion-icon name="trash-outline"> </ion-icon> <ion-icon name="create-outline" onclick="patchEmployee('tag_${dados[i]._id}');"> <ion-icon> 
-                </td>
+                <td><span class="${dados[i].category.color}"> ${dados[i].category.name} </td>
+                <td><ion-icon name="trash-outline"></ion-icon><ion-icon name="create-outline" onclick="patchEmployee('${dados[i]._id}');"><ion-icon></td>
             </tr>
             `
+            )
         }
     }
 }
 
-function atualizar(id) {
-    //fazer aparecer o modal
-    alert(id);
-
-}
-
+ajax.send();
 
 function patchEmployee(id) {
-    var url = `https://45gxmd-3000.preview.csb.app/tag`;
-    var res;
+    let url = `https://45gxmd-3000.preview.csb.app/tag`;
+    let xhttp = new XMLHttpRequest();
 
-    var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, false);
-    xhttp.send(); //A execução do script pára aqui até a requisição retornar do servidor
+    xhttp.send();
 
-    res = JSON.parse(xhttp.responseText);
+    let res = JSON.parse(xhttp.responseText);
 
-    document.getElementById("name_form_2").value = res[0].name;
-    document.getElementById("category_form_2").value = res[0].category.name;
-    document.getElementById("color_form_2").value = res[0].category.color;
-
-
-    document
-        .getElementById("patchButton")
-        .setAttribute("onClick", `javascript: patchEmployeeII(${id})`);
-
+    for(let i = 0; i < res.length; i++) {
+        if(res[i]._id == id) {
+            document.getElementById("id_form_2").value = res[i]._id;
+            document.getElementById("name_form_2").value = res[i].name;
+            document.getElementById("category_form_2").value = res[i].category.name;
+            document.getElementById("color_form_2").value = res[i].category.color;
+        }
+    }
     var patchModal = new bootstrap.Modal(document.getElementById("patchModal"), {
         keyboard: false,
     });
     patchModal.show();
 }
 
-function patchEmployeeII(_id) {
-    var name = document.getElementById("name_form_2").value;
-    var category = document.getElementById("category_form_2").value;
-    var color = document.getElementById("color_form_2").value;
-
-    var url = "https://45gxmd-3000.preview.csb.app/tag";
-
+function update() {
+    let id = document.getElementById("id_form_2").value;
+    console.log(typeof id);
+    let name = document.getElementById("name_form_2").value;
+    let category = document.getElementById("category_form_2").value;
+    let url = "https://45gxmd-3000.preview.csb.app/tag";
     $.ajax({
         type: "PATCH",
         url: url,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         data: JSON.stringify({
-            "_id": _id,
-            "name": name,
-            "category.name": category,
-            "category.color": color,
+            id: id,
+            name: name,
+            category: category
         }),
         success: function (res) {
             console.log(res);
+            window.location.reload();
         },
     });
-    window.location.reload(); //essa função mágica faz com que atualize a página, sem haver a necessidade de recarregar manualmente para ver a adição do profissional na tabela
 }
 
 
+let addCategory = document.getElementById('addCategory');
 
-ajax.send();
-
-
+addCategory.addEventListener('click', () => {
+    var createModal = new bootstrap.Modal(document.getElementById("createModal"), {
+        keyboard: false,
+    });
+    createModal.show();
+});
